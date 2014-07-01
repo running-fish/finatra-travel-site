1c6c399faf2aeaa6bbc1c2f23c4cb8c5b8d01146
andygodwin <andygodwin@gmail.com>
2014-07-01 21:14:04 +0100

    License and readme updates

--- src/main/scala/finatra/travel/api/App.scala
+++ src/main/scala/finatra/travel/api/App.scala
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2014 Andy Godwin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package finatra.travel.api
 
 import com.twitter.finatra._
@@ -6,6 +21,8 @@ import finatra.travel.api.controllers.HomeController
 import finatra.travel.api.services.{OffersService, LoyaltyService, ProfileService}
 
 object App extends FinatraServer {
+
+  System.setProperty("com.twitter.finatra.config.port", ":9100")
 
   val profileServiceHost = flag("profileServiceHost", "localhost:9200", "The host:port for the Profile Service")
   val profileServiceUrl = flag("profileServiceUrl", "/profile", "The base url for the Profile Service")
@@ -19,5 +36,5 @@ object App extends FinatraServer {
   val offersServiceUrl = flag("offersServiceUrl", "/offers", "The base url for the Offers Service")
   val offersService = new OffersService(offersServiceHost(), offersServiceUrl())
 
-  register(new HomeController(profileService, loyaltyService, offersService)
+  register(new HomeController(profileService, loyaltyService, offersService))
 }
