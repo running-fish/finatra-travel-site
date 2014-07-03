74080999b843a7e0c2564460b442177be49c4099
andygodwin <andygodwin@gmail.com>
2014-07-03 23:04:18 +0100

    Session trait for handling signed user session cookies

---
+++ src/main/scala/finatra/travel/api/controllers/Session.scala
@@ -0,0 +1,73 @@
+package finatra.travel.api.controllers
+
+import java.util.Base64
+import org.jboss.netty.util.CharsetUtil
+import com.twitter.finagle.http.{CookieMap, Cookie}
+import javax.crypto.spec.SecretKeySpec
+import javax.crypto.Mac
+
+/**
+ * Derived from https://github.com/mdennebaum/golden-arm/blob/master/src/main/scala/com/twitter/GoldenArm/utils/AuthUtils.scala
+ */
+trait Session {
+
+  val COOKIE_NAME = "GETAWAY_SESSION"
+
+  def fromCookies(secret:String, cookies: CookieMap): Option[String] = {
+    cookies.get(COOKIE_NAME) flatMap {
+      cookie => {
+        cookie.value.split("\\|") match {
+          case Array(encoded, timestamp, signature) if valid(encoded, timestamp, signature) => {
+            if (signaturesMatch(secret, encoded, timestamp, signature)) {
+              try {
+                Some(new String(Base64.getDecoder.decode(encoded.getBytes(CharsetUtil.UTF_8)), CharsetUtil.UTF_8))
+              } catch {
+                case iae: IllegalArgumentException => None
+              }
+            } else {
+              None
+            }
+          }
+          case _ => None
+        }
+      }
+    }
+  }
+
+  def toCookie(secret: String, id: String): Cookie = {
+    val timestamp = System.currentTimeMillis
+    val encoded = Base64.getEncoder.encodeToString(id.getBytes(CharsetUtil.UTF_8))
+    val signature = createSignature(secret, encoded, timestamp.toString)
+    new Cookie(COOKIE_NAME, encoded + "|" + timestamp.toString + "|" + signature)
+  }
+
+  // TODO: check encoded is valid Base64 alphabet?
+  // then the catch IllegalArgumentException will be unnecessary?
+  private def valid(encoded: String, timestamp: String, signature: String): Boolean = {
+    !encoded.isEmpty && !timestamp.isEmpty && timestamp.forall(_.isDigit) && !signature.isEmpty
+  }
+  
+  private def signaturesMatch(secret: String, encoded: String, timestamp: String, signature: String): Boolean = {
+    val check = createSignature(secret, encoded, timestamp.toString)
+    compare(signature.getBytes(CharsetUtil.UTF_8), check.getBytes(CharsetUtil.UTF_8))
+  }
+
+  private def createSignature(secret: String, encoded: String, timestamp: String) = {
+    val specSecret = new SecretKeySpec(secret.getBytes, "HmacSHA1")
+    val mac = Mac.getInstance("HmacSHA1")
+    mac.init(specSecret)
+    mac.update(encoded.getBytes(CharsetUtil.UTF_8))
+    mac.update(timestamp.getBytes(CharsetUtil.UTF_8))
+    mac.doFinal().mkString
+  }
+
+  private def compare(a: Array[Byte], b: Array[Byte]) = {
+    if (a.length != b.length) {
+      false
+    } else {
+      (0 until a.length).foldLeft(0) {
+        (acc, i) => acc | (a(i) ^ b(i))
+      } == 0
+    }
+  }
+}
