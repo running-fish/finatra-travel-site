2aa10802d7e8f169bc2f8ed913639664421d00d9
andygodwin <andygodwin@gmail.com>
2014-07-04 12:41:42 +0100

    Refactor duplicate code

--- src/test/scala/finatra/travel/api/controllers/SessionSpec.scala
+++ src/test/scala/finatra/travel/api/controllers/SessionSpec.scala
@@ -8,12 +8,12 @@ import org.mockito.Mockito._
 
 class SessionSpec extends FlatSpec with ShouldMatchers with Session with MockitoSugar {
 
+  val secret = "09vwi985e(*ht430)*!(T)JP"
+  val id = "945029340236"
+  val cookie = toCookie(secret, id)
+  val cookieValue = cookie.value
+
   "Session" should "validate signed cookie" in {
-    val secret = "09vwi985e(*ht430)*!(T)JP"
-    val id = "945029340236"
-
-    val cookie = toCookie(secret, id)
-
     val cookieMap = mock[CookieMap]
     when(cookieMap.get(COOKIE_NAME)).thenReturn(Some(cookie))
 
@@ -22,13 +22,7 @@ class SessionSpec extends FlatSpec with ShouldMatchers with Session with Mockito
   }
 
   it should "reject an empty encoded value" in {
-    val secret = "09vwi985e(*ht430)*!(T)JP"
-    val id = "945029340236"
-
-    val cookie = toCookie(secret, id)
-
-    val value = cookie.value
-    val emptyEncoded = value.substring(value.indexOf('|'))
+    val emptyEncoded = cookieValue.substring(cookieValue.indexOf('|'))
 
     val emptyEncodedCookie = new Cookie(COOKIE_NAME, emptyEncoded)
     val cookieMap = mock[CookieMap]
@@ -39,15 +33,9 @@ class SessionSpec extends FlatSpec with ShouldMatchers with Session with Mockito
   }
 
   it should "reject an empty timestamp value" in {
-    val secret = "09vwi985e(*ht430)*!(T)JP"
-    val id = "945029340236"
-
-    val cookie = toCookie(secret, id)
-
-    val value = cookie.value
-    val firstPipe = value.indexOf('|')
-    val secondPipe = value.indexOf('|', firstPipe + 1)
-    val emptyTimestamp = value.substring(0, firstPipe) + "|" + value.substring(secondPipe)
+    val firstPipe = cookieValue.indexOf('|')
+    val secondPipe = cookieValue.indexOf('|', firstPipe + 1)
+    val emptyTimestamp = cookieValue.substring(0, firstPipe) + "|" + cookieValue.substring(secondPipe)
 
     val emptyTimestampCookie = new Cookie(COOKIE_NAME, emptyTimestamp)
     val cookieMap = mock[CookieMap]
@@ -58,15 +46,9 @@ class SessionSpec extends FlatSpec with ShouldMatchers with Session with Mockito
   }
 
   it should "reject a value with only two elements" in {
-    val secret = "09vwi985e(*ht430)*!(T)JP"
-    val id = "945029340236"
-
-    val cookie = toCookie(secret, id)
-
-    val value = cookie.value
-    val firstPipe = value.indexOf('|')
-    val secondPipe = value.indexOf('|', firstPipe + 1)
-    val missingTimestamp = value.substring(0, firstPipe) + value.substring(secondPipe)
+    val firstPipe = cookieValue.indexOf('|')
+    val secondPipe = cookieValue.indexOf('|', firstPipe + 1)
+    val missingTimestamp = cookieValue.substring(0, firstPipe) + cookieValue.substring(secondPipe)
 
     val missingTimestampCookie = new Cookie(COOKIE_NAME, missingTimestamp)
     val cookieMap = mock[CookieMap]
@@ -77,15 +59,9 @@ class SessionSpec extends FlatSpec with ShouldMatchers with Session with Mockito
   }
 
   it should "reject an invalid timestamp" in {
-    val secret = "09vwi985e(*ht430)*!(T)JP"
-    val id = "945029340236"
-
-    val cookie = toCookie(secret, id)
-
-    val value = cookie.value
-    val firstPipe = value.indexOf('|')
-    val secondPipe = value.indexOf('|', firstPipe + 1)
-    val invalidTimestamp = value.substring(0, firstPipe) + "|109f8e5w9fw" + value.substring(secondPipe)
+    val firstPipe = cookieValue.indexOf('|')
+    val secondPipe = cookieValue.indexOf('|', firstPipe + 1)
+    val invalidTimestamp = cookieValue.substring(0, firstPipe) + "|109f8e5w9fw" + cookieValue.substring(secondPipe)
 
     val invalidTimestampCookie = new Cookie(COOKIE_NAME, invalidTimestamp)
     val cookieMap = mock[CookieMap]
@@ -96,15 +72,9 @@ class SessionSpec extends FlatSpec with ShouldMatchers with Session with Mockito
   }
 
   it should "reject a missing signature" in {
-    val secret = "09vwi985e(*ht430)*!(T)JP"
-    val id = "945029340236"
-
-    val cookie = toCookie(secret, id)
-
-    val value = cookie.value
-    val firstPipe = value.indexOf('|')
-    val secondPipe = value.indexOf('|', firstPipe + 1)
-    val missingSignature = value.substring(0, secondPipe) + "|"
+    val firstPipe = cookieValue.indexOf('|')
+    val secondPipe = cookieValue.indexOf('|', firstPipe + 1)
+    val missingSignature = cookieValue.substring(0, secondPipe) + "|"
 
     val missingSignatureCookie = new Cookie(COOKIE_NAME, missingSignature)
     val cookieMap = mock[CookieMap]
@@ -115,14 +85,7 @@ class SessionSpec extends FlatSpec with ShouldMatchers with Session with Mockito
   }
 
   it should "reject an invalid signature" in {
-    val secret = "09vwi985e(*ht430)*!(T)JP"
-    val id = "945029340236"
-
-    val cookie = toCookie(secret, id)
-
-    val value = cookie.value + "f"
-
-    val invalidSignatureCookie = new Cookie(COOKIE_NAME, value)
+    val invalidSignatureCookie = new Cookie(COOKIE_NAME, cookieValue + "f")
     val cookieMap = mock[CookieMap]
     when(cookieMap.get(COOKIE_NAME)).thenReturn(Some(invalidSignatureCookie))
 
