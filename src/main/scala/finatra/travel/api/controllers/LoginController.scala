0605da0738c0f4a41c74c558ff6b6cd76af8633c
andygodwin <andygodwin@gmail.com>
2014-07-04 12:35:42 +0100

    Generates html/json response based on Accept header

--- src/main/scala/finatra/travel/api/controllers/LoginController.scala
+++ src/main/scala/finatra/travel/api/controllers/LoginController.scala
@@ -15,11 +15,16 @@
  */
 package finatra.travel.api.controllers
 
-import com.twitter.finatra.Controller
+import com.twitter.finatra.{Request, ResponseBuilder, Controller}
 import finatra.travel.api.views.LoginView
-import finatra.travel.api.services.LoginService
+import finatra.travel.api.services.{User, LoginService}
+import com.twitter.finatra.ContentType.{Html, Json}
+import com.twitter.util.Future
 
 class LoginController(secret: String, loginService: LoginService) extends Controller with Session {
+
+  val invalidMessage = "Invalid username or password"
+  val errorMessage = "Sorry, but there was a problem verifying your credentials, please try again in a few minutes"
 
   get("/login") {
     request => {
@@ -29,21 +34,46 @@ class LoginController(secret: String, loginService: LoginService) extends Contro
   }
 
   post("/login") {
-    request => {
+    implicit request => {
       (request.params.get("username"), request.params.get("password")) match {
-        case (Some(username), Some(password)) => loginService.login(username, password) map {
-          user => {
-            user match {
-              case Some(u) => render.json(Map.empty).cookie(toCookie(secret, u.id))
-              case _ => {
-                val view = new LoginView(Some("Invalid username or password"))
-                render.view(view)
-              }
-            }
-          }
-        }
-        case _ => redirect("/").toFuture
+        case (Some(username), Some(password)) => login(username, password)
+        case _ => invalidLoginSubmission
       }
     }
+  }
+
+  private def login(username: String, password: String)(implicit request: Request): Future[ResponseBuilder] = {
+    loginService.login(username, password) flatMap {
+      user => verifyUser(user)
+    } rescue {
+      case e => loginException(e)
+    }
+  }
+
+  private def verifyUser(user: Option[User])(implicit request: Request): Future[ResponseBuilder] = {
+    user match {
+      case Some(u) => userFound(u)
+      case _ => userNotFound
+    }
+  }
+
+  private def userFound(user: User)(implicit request: Request): Future[ResponseBuilder] = respondTo(request) {
+    case _:Json => render.json(Map.empty).cookie(toCookie(secret, user.id)).toFuture
+    case _:Html => redirect("/").cookie(toCookie(secret, user.id)).toFuture
+  }
+
+  private def userNotFound()(implicit request: Request): Future[ResponseBuilder] = respondTo(request) {
+    case _:Json => render.status(404).json(Map("error" -> invalidMessage)).toFuture
+    case _:Html => render.view(new LoginView(Some(invalidMessage))).toFuture
+  }
+
+  private def invalidLoginSubmission()(implicit request: Request): Future[ResponseBuilder] = respondTo(request) {
+    case _:Json => render.status(400).toFuture
+    case _:Html => redirect("/").toFuture
+  }
+
+  private def loginException(e: Throwable)(implicit request: Request) = respondTo(request) {
+    case _:Json => render.status(500).json(Map("error" -> errorMessage)).toFuture
+    case _:Html => render.view(new LoginView(Some(errorMessage))).toFuture
   }
 }
